35f51a50e4acf1e4e92e828941e3baef7d0b83e9
Alexandre Paez Sequeira <alepaezseq@gmail.com>
2021-12-02 17:14:46 -0300

    create hpa for site

--- test/stress/site/stress.sh
+++ test/stress/site/stress.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 vegeta attack -rate=1000 -duration=1m -targets tests/site/targets.txt | vegeta report
+vegeta attack -rate=2000 -duration=1m -targets tests/site/targets.txt | vegeta report
+vegeta attack -rate=4000 -duration=1m -targets tests/site/targets.txt | vegeta report
+vegeta attack -rate=8000 -duration=1m -targets tests/site/targets.txt | vegeta report
+vegeta attack -rate=12000 -duration=1m -targets tests/site/targets.txt | vegeta report
